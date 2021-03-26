4a329de25f0d90f2de9411c3961b26ce8da26b64
Pedro Toledo <pedroltoledo@gmail.com>
2021-03-25 23:53:57 -0300

    ajustes no cache da geração de dados

--- pareto_front.py
+++ pareto_front.py
@@ -3,9 +3,13 @@ import pandas as pd
 import numpy as np
 import altair as alt
 
-def pareto_front(n: int, xobj:str = 'min', yobj: str = 'min') -> tuple:
+@st.cache
+def get_values(n: int) -> tuple:
     x = np.random.uniform(size = n)
     y = np.random.uniform(size = n)
+    return (x, y)
+
+def pareto_front(x: np.array, y: np.array, xobj: str = 'min', yobj: str = 'min') -> np.array:
     if xobj == 'max':
         order = np.argsort(x)[::-1]
     else:
@@ -29,9 +33,9 @@ def pareto_front(n: int, xobj:str = 'min', yobj: str = 'min') -> tuple:
                 pareto_opitimal.append(value)
             else:
                 continue
-    classification = np.repeat('Ponto Comum', n)
-    classification[pareto_opitimal] = 'Pareto'
-    return (x, y, np.array(classification))
+    classification = np.repeat('Non-Opitimal', n).astype('object')
+    classification[pareto_opitimal] = 'Pareto Opitimal'
+    return np.array(classification)
 
 '''
 # Finding the Pareto Front in 2-D Spaces
@@ -46,15 +50,16 @@ with col2:
     yobj = st.selectbox('Choose Y objective', ['min', 'max'], index = 0, format_func = lambda x: x.title())
 
 st.write("Here's what it looks like:")
-x, y, classe = pareto_front(n, xobj = xobj, yobj = yobj)
+x, y = get_values(n)
+classe = pareto_front(x, y, xobj = xobj, yobj = yobj)
 df = pd.DataFrame([(i, j, k) for i, j, k in zip(x, y, classe)], columns = ['x', 'y', 'classe'])
 
 with st.beta_container():
     plot = alt.Chart(df).mark_point().encode(
         x = 'x:Q', 
         y = 'y:Q', 
-        color = alt.Color('classe', scale=alt.Scale(scheme='set1')),
-        fill = alt.Color('classe', scale=alt.Scale(scheme='set1')), 
+        color = alt.Color('classe', scale=alt.Scale(domain=['Non-Opitimal', 'Pareto Opitimal'], range=["#377EB8", "#E41A1C"])),
+        fill = alt.Color('classe', scale=alt.Scale(domain=['Non-Opitimal', 'Pareto Opitimal'], range=["#377EB8", "#E41A1C"])), 
         tooltip = ['x', 'y']
     )
 
